8e8c98d5a473339b913aad86309ce2f3a7358bf3
lucabro81 <luca.brognara81@gmail.com>
2024-04-25 14:56:57 +0200

    fix: removed build folders

--- package/main.js
+++
@@ -1,7 +0,0 @@
-export class SnakeEngine {
-    constructor() { }
-    run() {
-        console.log("I'm runnig!");
-    }
-}
-//# sourceMappingURL=main.js.map
--- package/typings.d.ts
+++
@@ -1,3 +0,0 @@
-export class SnakeEngine {
-	run(): void;
-}
