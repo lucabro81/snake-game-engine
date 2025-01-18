49b530be2d1671e7d919145d5bfb16242cd9fe24
lucabro81 <luca.brognara81@gmail.com>
2025-01-18 23:46:34 +0100

    refactor: the same as before, I haven't added the new files

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+import { Vector2D } from "./core/types";
+
+export function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function toVector2D(position: string): Vector2D {
+  const [x, y] = position.split(',').map(Number);
+  return { x, y };
+}
